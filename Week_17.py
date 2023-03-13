Fixed numerical continuation

Week_17.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 def cubic(x, c):
     return x ** 3 - x + c
 
+c = np.linspace(-2, 2, 100)
+
 def param_cont(func=cubic):
-    sol_list=[]
+    sol_list = []
     u0 = 0
-    for c in range(-2,2):
-        sol = np.array(fsolve(func, u0, args=c))
-        sol_list.append(sol)
-        u0=sol
+    for i in c:
+        sol = fsolve(func, u0, args=(i,))
+        sol_list.append(sol[0])
+        u0 = sol[0]
     return np.array(sol_list)
 
-solutions=param_cont(func=cubic)
-plt.plot(solutions)
+solutions = param_cont(func=cubic)
+plt.plot(c, solutions)
+plt.xlabel('c')
+plt.ylabel('x')
 plt.show()