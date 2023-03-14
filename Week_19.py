6d29a99cd2831a26559a0bc00b2d15c7a2bc87a4
Josh Smith <de20812@bristol.ac.uk>
2023-03-14 16:20:05 +0000

    Included D

--- Week_19.py
+++ Week_19.py
@@ -44,32 +44,33 @@ u_true = true_ans(x, a, b, alpha, beta)
 
 ######################################################################
 
-def extendo_problem(u, N, dx, alpha, beta, f):
+def extendo_problem(u, N, dx, alpha, beta, f, D):
     F = np.zeros(N-1)
 
-    F[0] = (u[1] - 2*u[0] + alpha) / dx**2 - f[0]
+    F[0] = D*(u[1] - 2*u[0] + alpha) / dx**2 - f[0]
 
     for i in range(1, N-2):
-        F[i] = (u[i+1] - 2*u[i] + u[i-1]) / dx**2 - f[i]
+        F[i] = D*(u[i+1] - 2*u[i] + u[i-1]) / dx**2 - f[i]
 
-    F[N-2] = (beta - 2*u[N-2] + u[N-3]) / dx**2 - f[N-2]
+    F[N-2] = D*(beta - 2*u[N-2] + u[N-3]) / dx**2 - f[N-2]
 
     return F
 
-def true_extendo(x,a,b,alpha,beta):
-    answer = (-1/2)*(x-a)*(x-b) + ((beta-alpha)/(b-a))*(x-a)+alpha
+def true_extendo(x,a,b,alpha,beta,D):
+    answer = (-1/2*D)*(x-a)*(x-b) + ((beta-alpha)/(b-a))*(x-a)+alpha
     return np.array(answer)
 
+D=2
 u_guess = 0.1*x_int
 
-f=np.sin(x_int)
+f = np.zeros(len(x_int))
 
-sol = root(extendo_problem, u_guess, args = (N,dx,alpha,beta,f))
+sol = root(extendo_problem, u_guess, args = (N,dx,alpha,beta,f,D))
 
 print(sol.message)
 
 u_int = sol.x
-u_true = true_ans(x_int, a, b, alpha, beta)
+u_true = true_extendo(x_int, a, b, alpha, beta, D)
 
 plt.plot(x_int, u_int, 'o', label = "Numerical")
 plt.plot(x_int, u_true,'k',label='Exact')
