dc9e0655cf96f349f669a6d218619c6413d5c1ca
Josh Smith <de20812@bristol.ac.uk>
2023-03-21 18:37:22 +0000

    Attempt at part 3, solving the Bratu Problem

--- Week_19_matrices.py
+++ Week_19_matrices.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 def matrix_build(N,D):
     matrix = np.zeros(((N-1),(N-1)))
@@ -16,12 +17,13 @@ def boundary_conditions(N, alpha, beta):
     vector[-1] = beta
     return vector
     
-def source(N, D):
+def source(N, u):
     q = np.ones((N-1),)
+    q = q * np.exp(1)**u
     return q
 
-def true_ans(x,a,b,alpha,beta, D):
-    answer = (-1/(2*D))*((x-a)*(x-b)) + ((beta-alpha)/(b-a))*(x-a)+alpha
+def true_ans(x,a,b,alpha,beta, D, integer):
+    answer = (-integer)/(2*D)*((x-a)*(x-b)) + ((beta-alpha)/(b-a))*(x-a)+alpha
     return np.array(answer)
 
 def finite_grid(N, a, b):
@@ -30,7 +32,7 @@ def finite_grid(N, a, b):
     x_int=x[1:-1]
     return x, dx, x_int
 
-N=10
+N=20
 a=0
 b=1
 gamma1=0.0
@@ -38,18 +40,24 @@ gamma2=0.0
 x=np.linspace(a,b,N+1)
 dx=(b-a)/N
 x_int=x[1:-1]
-D = 2
+D = 1
 
-grid = finite_grid(N, 0, 1)
+grid = finite_grid(N, a, b)
 x = grid[0]
 dx = grid[1]
 x_int = grid[2]
-A_matrix = matrix_build(N,D)
-b_matrix = - ((dx)**2)*source(N,D) - A_matrix @ boundary_conditions(N,gamma1,gamma2)
-x_ans = np.linalg.solve(A_matrix, b_matrix)
-u_ans = true_ans(x_int, a, b, gamma1, gamma2, D)
 
-plt.plot(x_ans, 'o', label = "Numerical")
-plt.plot(u_ans,'k',label='Exact')
+u_old = 0
+for i in range(20):
+    integer = np.exp(1)**u_old
+    A_matrix = matrix_build(N,D)
+    b_matrix = - ((dx)**2)*source(N, integer) - A_matrix @ boundary_conditions(N,gamma1,gamma2)
+    u_new = np.linalg.solve(A_matrix, b_matrix)
+    u_ans = true_ans(x_int, a, b, gamma1, gamma2, D, integer)
+    u_old = u_new
+
+
+plt.plot(x[1:-1], u_new, 'o', label = "Numerical")
+plt.plot(x_int, u_ans,'k',label='Exact')
 plt.legend()
 plt.show()
