538074d025cc48e84969be40ed35a7c1046f59ad
Josh Smith <de20812@bristol.ac.uk>
2023-03-23 11:39:56 +0000

    Got the shooting code working

--- NumericalShooting.py
+++ NumericalShooting.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ODEsolver import solve_odes
 from scipy.signal import find_peaks
+from scipy.integrate import solve_ivp
+from scipy.optimize import fsolve
 
 def predator_prey(X, t, pars):
     """
@@ -10,7 +12,7 @@ def predator_prey(X, t, pars):
     :param X: Vector of (x, y) values
     :param t: Time value
     :param pars: Other paramters required to define the equation (a, b, d)
-    
+
     :returns: Array of derivatives dx/dt and dy/dt
     """
     x = X[0]
@@ -93,3 +95,29 @@ def isolate_orbit(func=predator_prey,  x0=[1, 1], t0=0, t1=200, dt_max=0.01, sol
         previous_value = x_values[i]
         previous_time = t[i]
     raise RuntimeError("No orbit found")
+
+def phase_condition(x0, *pars):
+    return predator_prey(x0, 0, *pars)[0]
+
+def shooting(f):
+    def G(u0T, pars):
+        def F(u0, T):
+            t_span = [1e-6, T]
+            t_eval = np.linspace(1e-6, T, 100)
+            sol = solve_ivp(fun=lambda t, X: f(X, t, pars), t_span=t_span, y0=u0, t_eval=t_eval, method='RK45')
+            final_sol = sol.y[:,-1]
+            return final_sol
+        T, u0 = u0T[-1], u0T[:-1]
+        return np.append(u0 - F(u0, T), phase_condition(u0, pars))
+    return G
+
+pars = [1.0, 0.5, 0.1]
+
+def find_shooting_orbit(f, u0T, pars):
+    fsolve_sol = fsolve(shooting(f), u0T, pars, full_output=True)
+    shooting_orbit = fsolve_sol[0]
+    return shooting_orbit
+
+pred_prey_u0T = np.array([0.2,0.2,13])
+found_shooting_orbit = find_shooting_orbit(predator_prey, pred_prey_u0T, pars)
+print(found_shooting_orbit)
