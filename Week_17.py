454a1b094d8a56c0f1d3800b883617846e603373
Josh Smith <de20812@bristol.ac.uk>
2023-03-23 17:38:38 +0000

    First attempt at param cont. for hopf eqn.

--- Week_17.py
+++ Week_17.py
@@ -2,12 +2,10 @@ import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 
-def cubic(x, c):
-    return x ** 3 - x + c
+def cubic(x, pars):
+    return x ** 3 - x + pars
 
-c = np.linspace(-2, 2, 100)
-
-def param_cont(func=cubic):
+def param_cont_cubic(func=cubic):
     sol_list = []
     u0 = 0
     for i in c:
@@ -16,8 +14,35 @@ def param_cont(func=cubic):
         u0 = sol[0]
     return np.array(sol_list)
 
-solutions = param_cont(func=cubic)
-plt.plot(c, solutions)
-plt.xlabel('c')
-plt.ylabel('x')
+c = np.linspace(-2, 2, 100)
+solutions = param_cont_cubic(func=cubic)
+#plt.plot(c, solutions)
+#plt.xlabel('c')
+#plt.ylabel('x')
+#plt.show()
+
+
+
+def hopf_bif(X, pars):
+    (u1, u2) = X
+    du1dt = (pars * u1) - u2 - (u1 * (u1**2 + u2**2))
+    du2dt = u1 + (pars * u2) - (u2 * (u1**2 + u2 ** 2))
+    return np.array([du1dt, du2dt])
+
+def param_cont_hopf(func=hopf_bif):
+    sol_list1 = []
+    sol_list2 = []
+    u0 = ([0,0])
+    for i in beta:
+        sol1, sol2 = fsolve(func, u0, args=(i,))
+        sol_list1.append(sol1)
+        sol_list2.append(sol2)
+        u0 = ([sol1,sol2])
+    return np.array(sol_list1), np.array(sol_list2)
+
+beta = np.linspace(0, 2, 100)
+sol_list1, sol_list2 = param_cont_hopf(func=hopf_bif)
+plt.plot(sol_list1, sol_list2, beta)
+plt.xlabel('sol_list1')
+plt.ylabel('sol_list2')
 plt.show()
