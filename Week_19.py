7e8948b0bba040a85a831bcc2974a147ba2bea5c
Josh Smith <de20812@bristol.ac.uk>
2023-03-31 13:07:24 +0100

    implemented neumann and robin bcs

--- Week_19.py
+++ Week_19.py
@@ -88,12 +88,25 @@ def matrix_build(N,D):
                 matrix[i][j]= D
     return matrix
 
-def boundary_conditions(N, alpha, beta):
+def dirichlet(N, alpha, beta):
     vector = np.zeros((N-1),)
     vector[0] = alpha
     vector[-1] = beta
     return vector
-    
+
+def Neumann(N, alpha, beta):
+    vector = np.zeros((N-1),)
+    vector[0] = alpha*dx
+    vector[-1] = beta*dx
+    return vector
+
+def robin(N, alpha, beta, pars):
+    gamma = pars[0]
+    vector = np.zeros((N-1),)
+    vector[0] = alpha*dx + gamma
+    vector[-1] = beta*dx - gamma
+    return vector
+
 def source(N, integer):
     q = np.ones((N-1),)
     q = q * integer
@@ -125,7 +138,7 @@ x = grid[0]
 dx = grid[1]
 x_int = grid[2]
 A_matrix = matrix_build(N,D)
-b_matrix = - ((dx)**2)*source(N, integer) - A_matrix @ boundary_conditions(N,gamma1,gamma2)
+b_matrix = - ((dx)**2)*source(N, integer) - A_matrix @ robin(N,gamma1,gamma2,b)
 x_ans = np.linalg.solve(A_matrix, b_matrix)
 u_ans = true_ans(x_int, a, b, gamma1, gamma2, D, integer)
 
@@ -134,63 +147,29 @@ plt.plot(u_ans,'k',label='Exact')
 plt.legend()
 plt.show()
 ################################################################################################
-def matrix_build(N,D):
-    matrix = np.zeros(((N-1),(N-1)))
-    np.fill_diagonal(matrix, -2*D)
-    for i in range((N-1)):
-        for j in range((N-1)):
-            if i == j-1 or i==j+1:
-                matrix[i][j]= D
-    return matrix
-
-def boundary_conditions(N, alpha, beta):
-    vector = np.zeros((N-1),)
-    vector[0] = alpha
-    vector[-1] = beta
-    return vector
-    
-def source(N, source_term):
-    q = np.ones((N-1),)
-    q = q * source_term
-    return q
-
-def true_ans(x,a,b,alpha,beta, D, source_term):
-    answer = (-source_term)/(2*D)*((x-a)*(x-b)) + ((beta-alpha)/(b-a))*(x-a)+alpha
-    return np.array(answer)
-
-def finite_grid(N, a, b):
+def solve_Bratu(N, a, b):
+    gamma1=0.0
+    gamma2=0.0
     x=np.linspace(a,b,N+1)
     dx=(b-a)/N
     x_int=x[1:-1]
-    return x, dx, x_int
-
-N=20
-a=0
-b=1
-gamma1=0.0
-gamma2=0.0
-x=np.linspace(a,b,N+1)
-dx=(b-a)/N
-x_int=x[1:-1]
-D = 1
-
-grid = finite_grid(N, a, b)
-x = grid[0]
-dx = grid[1]
-x_int = grid[2]
-
-u_old = np.zeros(N-1,)
-for i in range(20):
-    Bratu = np.exp(u_old*0.1)
-    A_matrix = matrix_build(N,D)
-    b_matrix = - ((dx)**2)*source(N, Bratu) - A_matrix @ boundary_conditions(N,gamma1,gamma2)
-    u_new = np.linalg.solve(A_matrix, b_matrix)
-    u_ans = true_ans(x_int, a, b, gamma1, gamma2, D, Bratu)
-    print(u_old)
-    u_old = u_new
-
-
-plt.plot(x[1:-1], u_new, 'o', label = "Numerical")
-plt.plot(x_int, u_ans,'k',label='Exact')
-plt.legend()
-plt.show()
+    D = 1
+    grid = finite_grid(N, a, b)
+    x = grid[0]
+    dx = grid[1]
+    x_int = grid[2]
+    u_old = np.zeros(N-1,)
+    for i in range(20):
+        Bratu = np.exp(u_old*0.1)
+        A_matrix = matrix_build(N,D)
+        b_matrix = - ((dx)**2)*source(N, Bratu) - A_matrix @ dirichlet(N,gamma1,gamma2)
+        u_new = np.linalg.solve(A_matrix, b_matrix)
+        u_ans = true_ans(x_int, a, b, gamma1, gamma2, D, Bratu)
+        print(abs(u_old - u_new))
+        u_old = u_new
+    plt.plot(x[1:-1], u_new, 'o', label = "Numerical")
+    plt.plot(x_int, u_ans,'k',label='Exact')
+    plt.legend()
+    plt.show()
+    
+solve_Bratu(20, 0, 1)
