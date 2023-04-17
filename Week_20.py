d289b77a89483ddf8425fd0379b642d4cab96d00
Josh Smith <de20812@bristol.ac.uk>
2023-04-17 16:08:10 +0100

    Broken down into functions for usability

--- Week_20.py
+++ Week_20.py
@@ -1,53 +1,59 @@
-from Week_19_matrices import *
 import numpy as np 
 import matplotlib.pyplot as plt
-from math import ceil
 import matplotlib.animation as animation
+from Week_19_matrices import matrix_build, boundary_conditions, finite_grid
 
-C = 0.9
-dt= C*dx**2/D
-t_final = 1
-N_time = int(np.ceil(t_final/dt))
-t = dt*np.arange(N_time)
+def heat_equation(N, D, gamma1, gamma2, a, b, dt, dx, t):
+    A_matrix = matrix_build(N,D)
+    b_matrix = A_matrix @ boundary_conditions(N,gamma1,gamma2)
+
+    U = np.zeros((N_time + 1,N-1))
+    U[0,:] = np.sin(np.pi*(x_int-a)/(b-a))
+    
+    for i in range(0,N_time-1):
+        U[i+1,:] = U[i,:] + (dt*D/dx**2)*(A_matrix@U[i,:]+b_matrix) 
+
+    u_true = np.zeros((N_time+1, N-1))
+    for n in range(0, N_time):
+        u_true[n,:] = np.exp(-(b-a)**2*D*np.pi**2*t[n]) * np.sin(np.pi*(x_int-a)/(b-a))
+    
+    return U, u_true
+
+def animate_solution(U, u_true, x_int, N_time):
+    fig, ax = plt.subplots()
+    ax.set_ylim(0,1)
+    ax.set_xlabel(f'$x$')
+    ax.set_ylabel(f'$u(x,t)$')
+
+    line, = ax.plot(x_int, u_true[0, :], label='True solution')
+    line2, = ax.plot(x_int, U[0, :], 'ro', label='Numerical solution')
+
+    def animate(i):
+        line.set_data((x_int, u_true[i,:]))
+        line2.set_data((x_int, U[i,:]))
+        return line, line2
+
+    ani = animation.FuncAnimation(fig, animate, frames=N_time, blit=False, interval=50)
+    ax.legend()
+    plt.show()
+
+def time_grid(N, a, b, C=0.49):
+    grid = finite_grid(N, a, b)
+    dx = grid[1]
+    x_int = grid[2]
+    dt = C*dx**2/D
+    t_final = 1
+    N_time = int(np.ceil(t_final/dt))
+    t = dt*np.arange(N_time)
+    return dt, dx, t, N_time, x_int
 
 N=20
 a=0
 b=1
 gamma1=0.0
 gamma2=0.0
-x=np.linspace(a,b,N+1)
-dx=(b-a)/N
-x_int=x[1:-1]
 D = 1
-integer = 1
 
-A_matrix = matrix_build(N,D)
-b_matrix = A_matrix @ boundary_conditions(N,gamma1,gamma2)
-
-U = np.zeros((N_time + 1,N-1))
-U[0,:] = np.sin(np.pi*(x_int-a)/(b-a))
-    
-for i in range(0,N_time-1):
-    U[i+1,:] = U[i,:] + (dt*D/dx**2)*(A_matrix@U[i,:]+b_matrix) 
-
-u_true = np.zeros((N_time+1, N-1))
-for n in range(0, N_time):
-    u_true[n,:] = np.exp(-(b-a)**2*D*np.pi**2*t[n]) * np.sin(np.pi*(x_int-a)/(b-a))
-
-# Plot the true solution
-fig, ax = plt.subplots()
-ax.set_ylim(0,1)
-ax.set_xlabel(f'$x$')
-ax.set_ylabel(f'$u(x,t)$')
-
-line, = ax.plot(x_int, u_true[0, :], label='True solution')
-line2, = ax.plot(x_int, U[0, :], 'ro', label='Numerical solution')
-
-def animate(i):
-    line.set_data((x_int, u_true[i,:]))
-    line2.set_data((x_int, U[i,:]))
-    return line, line2
-
-ani = animation.FuncAnimation(fig, animate, frames=N_time, blit=False, interval=50)
-ax.legend()
-plt.show()
+dt, dx, t, N_time, x_int = time_grid(N, a, b)
+U, u_true = heat_equation(N, D, gamma1, gamma2, a, b, dt, dx, t)
+animate_solution(U, u_true, x_int, N_time)
