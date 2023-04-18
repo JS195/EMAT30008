e3eba5c021128463dadb949aba8182355e7b68ac
Josh Smith <de20812@bristol.ac.uk>
2023-04-18 16:30:04 +0100

    Wrote the crank nicholson function

--- Week_21.py
+++ Week_21.py
@@ -4,7 +4,7 @@ from Week_19 import matrix_build, boundary_conditions
 from Week_20 import time_grid
 
 def b_euler(N, a, b, gamma1, gamma2, D):
-    dt, dx, t, N_time, x_int = time_grid(N, a, b)
+    dt, dx, t, N_time, x_int = time_grid(N, a, b, D)
     C = 0.1/dx**2
     A_matrix = matrix_build(N, D)
     b_matrix = A_matrix @ boundary_conditions(N, gamma1, gamma2)
@@ -17,8 +17,22 @@ def b_euler(N, a, b, gamma1, gamma2, D):
     
     return U, x_int
 
+def crank(N, a, b, gamma1, gamma2, D):
+    dt, dx, t, N_time, x_int = time_grid(N, a, b, D)
+    C = 0.1/dx**2
+    A_matrix = matrix_build(N, D)
+    b_matrix = A_matrix @ boundary_conditions(N, gamma1, gamma2)
+    identity = np.identity(N-1)
+    U = np.zeros((N_time + 1, N-1))
+    U[0,:] = np.sin(np.pi*x_int)
 
-def main()
+    for i in range(0, N_time-1):
+        U[i+1,:] = np.linalg.solve(identity - C/2 * A_matrix, (identity + C/2 * A_matrix) @ U[i,:] + C * b_matrix)
+    
+    return U, x_int
+
+def main():
+    # Defining some initial variables
     N = 20
     a = 0
     b = 1
@@ -26,9 +40,20 @@ def main()
     gamma2 = 0.0
     D = 0.1
 
-    # Plot the solution at each time step
+    # Part 1, backwards Euler demonstration
+    # Plot the solution for timestep t = 0
     U, x_int = b_euler(N, a, b, gamma1, gamma2, D)
     plt.plot(x_int, U[0,:], 'ro', label='solution for t=0')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('u')
+    plt.xlim(a, b)
+    plt.ylim(a, b)
+    plt.show()
+
+    # Part 2, crank nicholson method
+    U, x_int = crank(N, a, b, gamma1, gamma2, D)
+    plt.plot(x_int, U[0,:], 'bo', label='solution for t=0')
     plt.legend()
     plt.xlabel('x')
     plt.ylabel('u')
