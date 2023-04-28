3680abadbd9b6a15235558840b748716c3b3c00e
Josh Smith <de20812@bristol.ac.uk>
2023-04-28 12:33:34 +0100

    Solve BVP now iterative and can depend on solution

--- TEST.py
+++ TEST.py
@@ -1,45 +1,132 @@
 import numpy as np
-from PDEsolver import time_grid, animate_solution
-from BVPsolver import matrix_build, boundary_conditions, finite_grid
-from ExampleFunctions import linear_diffusion_IC
 import matplotlib.pyplot as plt
-import scipy.interpolate
-from ExampleFunctions import linear_diffusion_IC1
 
-def explicit_euler(N, D, gamma1, gamma2, a, b, dt, dx, t, N_time, x_int):
-    A_matrix = matrix_build(N,D)
-    b_matrix = A_matrix @ boundary_conditions(N,gamma1,gamma2)
+def matrix_build(N,D):
+    matrix = np.zeros(((N-1),(N-1)))
+    np.fill_diagonal(matrix, -2)
+    for i in range((N-1)):
+        for j in range((N-1)):
+            if i == j-1 or i==j+1:
+                matrix[i][j]= 1
+    return matrix*D
 
-    U = np.zeros((N_time + 1, N-1))
-    U[0,:] = linear_diffusion_IC1(x_int, a, b)
-    
-    for i in range(0,N_time-1):
-        U[i+1,:] = U[i,:] + (dt*D/dx**2)*(A_matrix@U[i,:]+b_matrix) 
+def dirichlet(N, alpha, beta):
+    vector = np.zeros((N-1),)
+    vector[0] = alpha
+    vector[-1] = beta
+    return vector
 
-    return U
+def neumann(N, alpha, beta, dx):
+    A = np.zeros((N-1, N-1))
+    A[0, 0] = -1
+    A[0, 1] = 1
+    A[-1, -2] = -1
+    A[-1, -1] = 1
+    return A * alpha / dx
 
-def true_sol_func(N, D, a, b, t, N_time, x_int):
-    u_true = np.zeros((N_time+1, N-1))
-    for n in range(0, N_time):
-        u_true[n,:] = np.exp(-(b-a)**2*D*np.pi**2*t[n]) * np.sin(np.pi*(x_int-a)/(b-a))
-    return u_true
+def robin(N, gamma1, gamma2, k_a, k_b, h_a, h_b, dx):
+    A = np.zeros((N-1, N-1))
+    A[0, 0] = k_a + h_a * dx
+    A[0, 1] = -k_a
+    A[-1, -2] = -k_b
+    A[-1, -1] = k_b + h_b * dx
+    return A
+
+def source_function(N, integer, x_int, u, mu, x_dependent, u_dependent):
+    if x_dependent:
+        q = integer * x_int
+    else:
+        q = np.ones((N-1),) * integer
+
+    if u_dependent:
+        q *= (u + mu)
+
+    return q
 
 
-def heat_equation_RK4(N, D, gamma1, gamma2, a, b, dt, dx, t, x_int):
-    A_matrix = matrix_build(N,D)
-    b_matrix = A_matrix @ boundary_conditions(N,gamma1,gamma2)
+def true_sol(x,a,b,alpha,beta, D):
+    answer = ((beta - alpha)/(b - a))*(x - a) + alpha
+    return np.array(answer)
 
-    U = np.zeros((len(t), N-1))
-    U[0,:] = np.sin(np.pi*(x_int-a)/(b-a))
-    
-    def f(U, t):
-        return D/dx**2 * (A_matrix @ U + b_matrix)
-    
-    for i in range(len(t)-1):
-        U[i+1,:], _ = RK4_step(f, U[i,:], t[i], dt)
+def true_ans_part2(x,a,b,alpha,beta, D, integer):
+    answer = (-integer)/(2*D)*((x-a)*(x-b)) + ((beta-alpha)/(b-a))*(x-a)+alpha
+    return np.array(answer)
 
-    u_true = np.zeros((len(t), N-1))
-    for n in range(len(t)):
-        u_true[n,:] = np.exp(-(b-a)**2*D*np.pi**2*t[n]) * np.sin(np.pi*(x_int-a)/(b-a))
-    
-    return U, u_true
+def finite_grid(N, a, b):
+    x=np.linspace(a,b,N+1)
+    dx=(b-a)/N
+    x_int=x[1:-1]
+    return x, dx, x_int
+
+def BVP_solver(N, a, b, gamma1, gamma2, D, integer=1, mu=1, source=None, boundary="dirichlet", k_a=None, k_b=None, h_a=None, h_b=None, x_dependant=False, u_dependant=False, tol=1e-6, max_iter=100):
+    grid = finite_grid(N, a, b)
+    x = grid[0]
+    dx = grid[1]
+    x_int = grid[2]
+    A_matrix = matrix_build(N, D)
+
+    if boundary == "dirichlet":
+        b_matrix = -dirichlet(N, gamma1, gamma2)
+    elif boundary == "neumann":
+        b_matrix = np.zeros(N-1)
+        A_matrix += neumann(N, gamma1, gamma2, dx)
+    elif boundary == "robin":
+        if k_a is None or k_b is None or h_a is None or h_b is None:
+            raise ValueError("k_a, k_b, h_a, and h_b must be provided for Robin boundary conditions.")
+        b_matrix = np.zeros(N-1)
+        A_matrix += robin(N, gamma1, gamma2, k_a, k_b, h_a, h_b, dx)
+
+    u = np.zeros(N-1)
+    u_prev = np.ones(N-1) * np.inf
+    iter_count = 0
+
+    while np.linalg.norm(u - u_prev) > tol and iter_count < max_iter:
+        u_prev = u.copy()
+
+        if source:
+            b_matrix_updated = b_matrix - ((dx)**2) * source_function(N, integer, x_int, u, mu, x_dependant, u_dependant)
+            u = np.linalg.solve(A_matrix, b_matrix_updated)
+        else:
+            u = np.linalg.solve(A_matrix, b_matrix)
+        
+        iter_count += 1
+
+    if iter_count == max_iter:
+        print("Warning: Maximum number of iterations reached. Solution may not have converged.")
+
+    return u, x_int
+
+
+N = 50
+a = 0
+b = 1
+gamma1 = 0.0
+gamma2 = 1.0
+D = 1
+
+# Part 1
+x_ans, x_int = BVP_solver(N, a, b, gamma1, gamma2, D, source=False)
+u_true = true_sol(x_int, a, b, gamma1, gamma2, D)
+
+plt.plot(x_int, x_ans, 'o', label="Numerical")
+plt.plot(x_int, u_true, 'k', label='Exact')
+plt.legend()
+plt.show()
+
+# Part 2
+gamma2 = 0.0
+x_ans2, x_int2 = BVP_solver(N, a, b, gamma1, gamma2, D, source=source_function)
+u_ans2 = true_ans_part2(x_int2, a, b, gamma1, gamma2, D, 1)
+
+plt.plot(x_int2, x_ans2, 'o', label="Numerical")
+plt.plot(x_int2, u_ans2, 'k', label='Exact')
+plt.legend()
+plt.show()
+
+
+x_ansbratu, x_intbratu = BVP_solver(N, a, b, gamma1, gamma2, D, mu=0.1, source=True, boundary="dirichlet", x_dependant=False, u_dependant=True, tol=1e-6, max_iter=100)
+
+plt.plot(x_intbratu, x_ansbratu, 'o', label="Numerical")
+
+plt.legend()
+plt.show()
---
+++ TEST2.py
@@ -0,0 +1,140 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def matrix_build(N,D):
+    matrix = np.zeros(((N-1),(N-1)))
+    np.fill_diagonal(matrix, -2)
+    for i in range((N-1)):
+        for j in range((N-1)):
+            if i == j-1 or i==j+1:
+                matrix[i][j]= 1
+    return matrix*D
+
+def dirichlet(N, alpha, beta):
+    vector = np.zeros((N-1),)
+    vector[0] = alpha
+    vector[-1] = beta
+    return vector
+
+def neumann(N, alpha, beta, dx):
+    A = np.zeros((N-1, N-1))
+    A[0, 0] = -1
+    A[0, 1] = 1
+    A[-1, -2] = -1
+    A[-1, -1] = 1
+    return A * alpha / dx
+
+def robin(N, gamma1, gamma2, k_a, k_b, h_a, h_b, dx):
+    A = np.zeros((N-1, N-1))
+    A[0, 0] = k_a + h_a * dx
+    A[0, 1] = -k_a
+    A[-1, -2] = -k_b
+    A[-1, -1] = k_b + h_b * dx
+    return A
+
+def true_sol(x,a,b,alpha,beta, D):
+    answer = ((beta - alpha)/(b - a))*(x - a) + alpha
+    return np.array(answer)
+
+def true_ans_part2(x,a,b,alpha,beta, D, integer):
+    answer = (-integer)/(2*D)*((x-a)*(x-b)) + ((beta-alpha)/(b-a))*(x-a)+alpha
+    return np.array(answer)
+
+def finite_grid(N, a, b):
+    x=np.linspace(a,b,N+1)
+    dx=(b-a)/N
+    x_int=x[1:-1]
+    return x, dx, x_int
+
+def source_function(N, integer, x_int, u, x_dependent, u_dependent):
+    if x_dependent:
+        q = x_int*integer
+    else:
+        q = np.ones((N-1),)*integer
+
+    if u_dependent:
+        q *= (u + integer)
+
+    return q
+
+def Matrix_solver(N, A_matrix, dx, x_int, b_matrix, x_dependant=False, integer=1, source=None):
+    if source:
+        b_matrix -= ((dx)**2) * source_function(N, integer, x_int, 0, x_dependant, u_dependent=False)
+
+    x_ans = np.linalg.solve(A_matrix, b_matrix)
+    return x_ans, x_int
+
+def Iterative_solver(N, A_matrix, dx, x_int, b_matrix, integer, source, x_dependant, u_dependant, tol, max_iter):
+    u = np.zeros(N-1)
+    u_prev = np.ones(N-1) * np.inf
+    iter_count = 0
+
+    while np.linalg.norm(u - u_prev) > tol and iter_count < max_iter:
+        u_prev = u.copy()
+
+        if source:
+            b_matrix_updated = b_matrix - ((dx)**2) * source_function(N, integer, x_int, u, x_dependant, u_dependant)
+            u = np.linalg.solve(A_matrix, b_matrix_updated)
+        else:
+            u = np.linalg.solve(A_matrix, b_matrix)
+
+        iter_count += 1
+    if iter_count == max_iter:
+        print("Warning: Maximum number of iterations reached. Solution may not have converged.")
+    return(u, x_int)
+
+
+
+
+def BVP_solver(N, a, b, gamma1, gamma2, D, integer=1, source=None, boundary="dirichlet", k_a=None, k_b=None, h_a=None, h_b=None, x_dependant=False, u_dependant=False, tol=1e-6, max_iter=100):
+    grid = finite_grid(N, a, b)
+    x = grid[0]
+    dx = grid[1]
+    x_int = grid[2]
+    A_matrix = matrix_build(N, D)
+
+    if boundary == "dirichlet":
+        b_matrix = -dirichlet(N, gamma1, gamma2)
+    elif boundary == "neumann":
+        b_matrix = np.zeros(N-1)
+        A_matrix += neumann(N, gamma1, gamma2, dx)
+    elif boundary == "robin":
+        if k_a is None or k_b is None or h_a is None or h_b is None:
+            raise ValueError("k_a, k_b, h_a, and h_b must be provided for Robin boundary conditions.")
+        b_matrix = np.zeros(N-1)
+        A_matrix += robin(N, gamma1, gamma2, k_a, k_b, h_a, h_b, dx)
+
+    if u_dependant == False:
+        x_ans, x_int = Matrix_solver(N, A_matrix, dx, x_int, b_matrix, x_dependant, integer, source)
+
+    else:
+        x_ans, x_int = Iterative_solver(N, A_matrix, dx, x_int, b_matrix, integer, source, x_dependant, u_dependant, tol, max_iter)
+
+    return x_ans, x_int
+
+# Part 1
+x_ans, x_int = BVP_solver(N=50, a=0, b=1, gamma1=0, gamma2=1, D=1, source=False)
+u_true = true_sol(x_int, a=0, b=1, alpha=0, beta=1, D=1)
+
+plt.plot(x_int, x_ans, 'o', label="Numerical")
+plt.plot(x_int, u_true, 'k', label='Exact')
+plt.legend()
+plt.show()
+
+# Part 2
+gamma2 = 0.0
+x_ans2, x_int2 = BVP_solver(N=50, a=0, b=1, gamma1=0, gamma2=0, D=1, integer=1, source=True, boundary="dirichlet", x_dependant=False)
+u_ans2 = true_ans_part2(x_int2, a=0, b=1, alpha=0, beta=0, D=1, integer=1)
+
+plt.plot(x_int2, x_ans2, 'o', label="Numerical")
+plt.plot(x_int2, u_ans2, 'k', label='Exact')
+plt.legend()
+plt.show()
+
+
+x_ansbratu, x_intbratu = BVP_solver(N=50, a=0, b=1, gamma1=0, gamma2=0, D=1, integer=0.1, source=True, boundary="dirichlet", x_dependant=False, u_dependant=True, tol=1e-6, max_iter=100)
+
+plt.plot(x_intbratu, x_ansbratu, 'o', label="Numerical")
+
+plt.legend()
+plt.show()
